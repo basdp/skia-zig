5fae5d443d0d6b9a7aed6495f66ad1b80b8dfb0d
Bas du Pré <me@basdupre.com>
2024-10-09 17:27:48 +0200

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,12 @@
 const c = @cImport({
-    @cInclude("skia/include/c/gr_context.h");
-    @cInclude("skia/include/c/sk_canvas.h");
-    @cInclude("skia/include/c/sk_colorspace.h");
-    @cInclude("skia/include/c/sk_data.h");
-    @cInclude("skia/include/c/sk_image.h");
-    @cInclude("skia/include/c/sk_paint.h");
-    @cInclude("skia/include/c/sk_path.h");
-    @cInclude("skia/include/c/sk_surface.h");
+    @cInclude("../skia/include/c/gr_context.h");
+    @cInclude("../skia/include/c/sk_canvas.h");
+    @cInclude("../skia/include/c/sk_colorspace.h");
+    @cInclude("../skia/include/c/sk_data.h");
+    @cInclude("../skia/include/c/sk_image.h");
+    @cInclude("../skia/include/c/sk_paint.h");
+    @cInclude("../skia/include/c/sk_path.h");
+    @cInclude("../skia/include/c/sk_surface.h");
 });
 
 pub usingnamespace c;
