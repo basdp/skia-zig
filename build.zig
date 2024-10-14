b43ea8444f197d03b13f1ce4262bc2325f89b0b5
Bas du Pré <me@basdupre.com>
2024-10-14 13:06:51 +0200

    Update build.yml and build.zig

--- build.zig
+++ build.zig
@@ -33,7 +33,6 @@ pub fn build(b: *std.Build) !void {
         },
         else => return error.PlatformNotSupported,
     }
-    module.addIncludePath(b.path("skia"));
     module.linkSystemLibrary("skia", .{ .preferred_link_mode = .static });
     module.link_libc = true;
 }
